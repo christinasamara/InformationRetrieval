244e32d3e243fe653c9a59dc99bd35cdf7e69fb0
christinasamara <christinaesamara@gmail.com>
2023-10-24 11:40:41 +0300

    11.40

--- inverted_index.py
+++ inverted_index.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import numpy as np
+from nltk.corpus import stopwords
 
 tokens = []
 inverted_index = {}
@@ -14,12 +15,19 @@ for filename in glob.glob("C:\\Users\\chris\\Downloads\\ir_project\\docs\*"):
 for i in range(len(tokens)):
     for index, token in enumerate(tokens[i]):
         if token not in inverted_index:
-            inverted_index[token] = [i, 1, [index]]
+            ins_dict = {}
+            ins_dict[i] = [1, [index]] 
+            inverted_index[token] = ins_dict
         else:
-            if inverted_index[token][0] == i!
+            for j in list(inverted_index[token].keys()):
+                if ( j == i ):
+                    inverted_index[token][j][0] += 1
+                    inverted_index[token][j][1].append(index)
+                else:
+                    inverted_index[token][i] = [1, [index]]
 
-        inverted_index[token][1] +=1
-        inverted_index[token][2].append(index)
+for key, value in inverted_index.items():
+    print(key, ":", value)
 
 
         
