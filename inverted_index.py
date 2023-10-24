74f412572528e73070d3d699a8e738e97ce880fa
christinasamara <christinaesamara@gmail.com>
2023-10-24 12:32:48 +0300

    stopwords

--- inverted_index.py
+++ inverted_index.py
@@ -1,8 +1,11 @@
 import glob
 import os
 import numpy as np
-from nltk.corpus import stopwords
+import nltk 
+ 
 
+nltk.download('stopwords')
+stop_words = nltk.corpus.stopwords.words('english')
 tokens = []
 inverted_index = {}
 flag = 0
@@ -14,17 +17,18 @@ for filename in glob.glob("C:\\Users\\chris\\Downloads\\ir_project\\docs\*"):
 
 for i in range(len(tokens)):
     for index, token in enumerate(tokens[i]):
-        if token not in inverted_index:
-            ins_dict = {}
-            ins_dict[i] = [1, [index]] 
-            inverted_index[token] = ins_dict
-        else:
-            for j in list(inverted_index[token].keys()):
-                if ( j == i ):
-                    inverted_index[token][j][0] += 1
-                    inverted_index[token][j][1].append(index)
-                else:
-                    inverted_index[token][i] = [1, [index]]
+        if token not in stop_words and len(token)>3:
+            if token not in inverted_index:
+                ins_dict = {}
+                ins_dict[i] = [1, [index]] 
+                inverted_index[token] = ins_dict
+            else:
+                for j in list(inverted_index[token].keys()):
+                    if ( j == i ):
+                        inverted_index[token][j][0] += 1
+                        inverted_index[token][j][1].append(index)
+                    else:
+                        inverted_index[token][i] = [1, [index]]
 
 for key, value in inverted_index.items():
     print(key, ":", value)
