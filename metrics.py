d15f1cf4a1786c2e832a4beae48fd098c532b378
christinasamara <christinaesamara@gmail.com>
2023-12-10 17:24:47 +0200

    5.24

--- metrics.py
+++ metrics.py
@@ -8,10 +8,12 @@ relevant_docs = []
 K = 20
 
 def initialization(file):
+    results = [] 
     f = open(file, "r")
     lines = f.read().split("\n")
     for line in lines:
         results.append(line.split(" "))
+    return results
 
 def open_relevants(file):
     f = open(file, "r")
@@ -19,7 +21,7 @@ def open_relevants(file):
     for t in text:
         relevant_docs.append(t)
 
-def precisions(QUERY):
+def precisions(QUERY, results):
     count_relevant = 1
     sum_precisions = 0
     for rank, doc in enumerate(results[QUERY], 1):
@@ -31,16 +33,33 @@ def precisions(QUERY):
     average_precision = sum_precisions / count_relevant
     return average_precision
 
-open_relevants("Relevant_20")
-f = open("Queries_20")
 
-# initialization("tfc_results.txt")
+def ap_results_tfc(file, query_file):
+    f = open(file, "w")
+    q = open(query_file, "r")
+    results = initialization("tfc_results.txt")
+    for query in range(len(q.read().split("\n"))):
+        f.write(str(precisions(query, results)))
+        f.write("\n")
+
+
+def ap_results_txc(file, query_file):
+    f = open(file, "w")
+    q = open(query_file, "r")
+    results = initialization("txc_results.txt")
+    for query in range(len(q.read().split("\n"))):
+        f.write(str(precisions(query, results)))
+        f.write("\n")
+
+
+open_relevants("Relevant_20")
+ap_results_tfc("ap_results_tfc.txt", "Queries_20")
+ap_results_txc("ap_results_txc.txt", "Queries_20")
+
+# initialization("txc_results.txt")
 # for query in range(len(f.read().split("\n"))):
 #     print(precisions(query))
 
-initialization("txc_results.txt")
-for query in range(len(f.read().split("\n"))):
-    print(precisions(query))
 
 
 
